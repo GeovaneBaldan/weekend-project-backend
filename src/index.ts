c2b66c08f4512591cc885bf34c1556ea513b6235
Gabriel Vander <gabsvander@hotmail.com>
2020-04-06 23:11:46 -0300

    [SETUP-0001] Adds a configurable MongoDB Atlas datasource

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import {WeekendProjectBackendApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import {DATASOURCE_NAME, DATASOURCE_PREFIX, DB_URL} from './const';
 
 export {WeekendProjectBackendApplication};
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new WeekendProjectBackendApplication(options);
+
+  const datasourceDb = require(`./datasources/${DATASOURCE_PREFIX}.datasource.config.json`);
+
+  const updatedDatasourceDb = Object.assign({}, datasourceDb, {
+    url: DB_URL,
+  });
+
+  app.bind(`datasources.config.${DATASOURCE_NAME}`).to(updatedDatasourceDb);
+
   await app.boot();
   await app.start();
 
